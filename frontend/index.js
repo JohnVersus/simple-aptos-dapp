2f76dc7fc46fd749269c085924cc2c5488ac55cb
John Versus <johnvs.nagendra@gmail.com>
2023-01-05 09:51:13 +0530

    init - wallet connect and sign message

---
+++ frontend/index.js
@@ -0,0 +1,93 @@
+const isPetraInstalled = window.aptos;
+
+const getAptosWallet = () => {
+  if ("aptos" in window) {
+    return window.aptos;
+  } else {
+    window.open("https://petra.app/", `_blank`);
+  }
+};
+
+const signAndVerify = async (message) => {
+  try {
+    const response = await window.aptos.signMessage({
+      message,
+      nonce: Math.ceil(Math.random() * 100000),
+      application: true,
+      address: true,
+      chainId: true,
+    });
+    const { publicKey } = await window.aptos.account();
+    // Remove the 0x prefix
+    const key = publicKey?.slice(2, 66);
+    const verified = nacl.sign.detached.verify(
+      buffer.Buffer.from(response.fullMessage),
+      buffer.Buffer.from(response.signature, "hex"),
+      buffer.Buffer.from(key, "hex")
+    );
+    console.log(verified);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const connect = async () => {
+  const wallet = getAptosWallet();
+  console.log(wallet);
+  try {
+    const response = await wallet.connect();
+    console.log(response); // { address: string, publicKey: string }
+
+    const message = "Welcome to my dapp!";
+    await signAndVerify(message);
+
+    const account = await wallet.account();
+    console.log(account); // { address: string, publicKey: string }
+
+    localStorage.setItem("userAccount", JSON.stringify(account));
+
+    refreshUi();
+  } catch (error) {
+    // { code: 4001, message: "User rejected the request."}
+    console.log(error);
+  }
+};
+
+const showUserData = () => {
+  const account = localStorage.getItem("userAccount");
+  console.log(account);
+  if (account !== "{}" && account !== null) {
+    walletInfo.innerText = JSON.stringify(JSON.parse(account), null, 4);
+  } else {
+    walletInfo.innerText = "Wallet Not Connected!!";
+  }
+};
+
+const toggleBtn = async () => {
+  const account = localStorage.getItem("userAccount");
+  if (account !== "{}" && account !== null) {
+    connectButton.disabled = true;
+    disconnectButton.disabled = false;
+  } else {
+    connectButton.disabled = false;
+    disconnectButton.disabled = true;
+  }
+};
+
+const refreshUi = () => {
+  console.log("Ui Updated");
+  showUserData();
+  toggleBtn();
+};
+
+const disconnect = async () => {
+  const wallet = getAptosWallet();
+  await wallet.disconnect();
+  localStorage.setItem("userAccount", "{}");
+  refreshUi();
+};
+
+const init = async () => {
+  refreshUi();
+};
+init();
---
+++ frontend/tx.js
@@ -0,0 +1,29 @@
+const sendTx = async () => {
+  const wallet = getAptosWallet(); // see "Connecting"
+
+  // Example Transaction, following an [EntryFunctionPayload](https://github.com/aptos-labs/aptos-core/blob/main/ecosystem/typescript/sdk/src/generated/models/EntryFunctionPayload.ts#L8-L21)
+  const address =
+    "0x17099e697e0ca0426b18c69a57fd715a5830538387933b13e470adff9f8e1c49";
+  const transaction = {
+    arguments: [address, "717"],
+    function: "0x1::coin::transfer",
+    type: "entry_function_payload",
+    type_arguments: ["0x1::aptos_coin::TestCoin"],
+  };
+
+  try {
+    const pendingTransaction = await window.aptos.signAndSubmitTransaction(
+      transaction
+    );
+
+    // In most cases a dApp will want to wait for the transaction, in these cases you can use the typescript sdk
+    const client = new AptosClient("https://testnet.aptoslabs.com");
+    const txn = await client.waitForTransactionWithResult(
+      pendingTransaction.hash
+    );
+    console.log(txn);
+  } catch (error) {
+    // see "Errors"
+    console.log(error);
+  }
+};
